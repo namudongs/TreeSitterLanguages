6b730e9c41acca5137c8bee27a21972576406a10
namudi <namudongs@gmail.com>
2025-05-07 20:03:48 +0900

    Fixed Swift Indentation

--- Sources/TreeSitterSwiftRunestone/TreeSitterIndentationScopes+Helpers.swift
+++ Sources/TreeSitterSwiftRunestone/TreeSitterIndentationScopes+Helpers.swift
@@ -1,25 +1,37 @@
 import Runestone
 
-public extension TreeSitterIndentationScopes {
-    static var swift: TreeSitterIndentationScopes {
-        return TreeSitterIndentationScopes(
-            indent: [
-                "protocol_body",
-                "class_body",
-                "computed_property",
-                "computed_getter",
-                "computed_setter",
-                "function_body",
-                "if_statement",
-                "lambda_literal",
-                "array_literal",
-                "dictionary_literal",
-                "switch_entry"
-            ],
-            outdent: [
-                "else",
-                "}",
-                "]"
-            ])
-    }
+extension TreeSitterIndentationScopes {
+  public static var swift: TreeSitterIndentationScopes {
+    return TreeSitterIndentationScopes(
+      indent: [
+        "protocol_body",
+        "class_body",
+        "computed_property",
+        "computed_getter",
+        "computed_setter",
+        "function_body",
+        "if_statement",
+        "lambda_literal",
+        "array_literal",
+        "dictionary_literal",
+        "switch_entry",
+        "for_statement",
+        "while_statement",
+        "repeat_while_statement",
+        "enum_class_body",
+        "do_statement",
+        "catch_block",
+        "guard_statement",
+        "willset_clause",
+        "didset_clause",
+      ],
+      outdent: [
+        "else",
+        "}",
+        "]",
+        "catch_keyword",
+        "case",
+        "default_keyword",
+      ])
+  }
 }
